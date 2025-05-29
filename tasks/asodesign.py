ce3863eaa04ae7182d43b1104ab209a023d5a13a
Planet-KIM <kdw59520@gmail.com>
2025-05-29 21:39:51 +0900

    [fix] fix the celery app name and Make the more informations

--- tasks/asodesign.py
+++ tasks/asodesign.py
@@ -3,12 +3,15 @@
 import multiprocessing as mp
 import time
 import sys
+import os
 sys.path.append("/Users/dowonkim/Dropbox/data/celery_asodesign/mypkg/")
 from asopipe.main import run_ASOdesign#ASOdesign
-from app import celery
+from app import celery_app
+
+from celery import current_task, shared_task
 
 
-@celery.task
+@celery_app.task
 def run_asodesign_process(
     transid="NM_002415",
     query_assemblies=["mm39", "mm10"],
@@ -24,22 +27,22 @@ def run_asodesign_process(
     start = time.perf_counter()
     chunk_division = 5
     max_workers = 1
-    print(
-        f"transid: {transid}, query_assemblies: {query_assemblies}, ref_assembly: {ref_assembly}, "
-        f"k_min: {k_min}, k_max: {k_max}, chunk_division: {chunk_division}, max_workers: {max_workers}, wobble: {wobble}, gapmer_filtered: {gapmer_filtered}",
-        type(transid), type(query_assemblies), type(ref_assembly), type(k_min), type(k_max), type(chunk_division), type(max_workers), type(wobble), type(gapmer_filtered)
-    )
+
+    task_id = current_task.request.id
+    output_path = f"/Users/dowonkim/Dropbox/data/celery_asodesign/asoview/design_folder/{task_id}/"
+    if not os.path.exists(output_path):
+        os.makedirs(output_path, exist_ok=True)
     result  = run_ASOdesign(transid=transid,
                     query_assembly=query_assemblies,
                     ref_assembly=ref_assembly,
                     k_min=k_min, k_max=k_max,
                     chunk_division=chunk_division, max_workers=max_workers, wobble=wobble, to_df=False, gapmer_filtered=gapmer_filtered,
-                    to_csv=True, output_path=f"/Users/dowonkim/Dropbox/data/celery_asodesign/asoview/design_folder/")
-    print(result)
+                    to_csv=True, output_path=output_path)
 
     return {
         "elapsed_sec": round(time.perf_counter() - start, 3),
         "records": result,
+        "id": task_id
     }
     
     
