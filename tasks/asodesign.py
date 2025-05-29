9ec71bed5b74247f6e59c5d94abcbf476fe87b74
Planet-KIM <kdw59520@gmail.com>
2025-05-29 19:30:45 +0900

    [fix] fix to match argument

--- tasks/asodesign.py
+++ tasks/asodesign.py
@@ -2,42 +2,44 @@
 
 import multiprocessing as mp
 import time
-from asopipe.main import ASOdesign
+import sys
+sys.path.append("/Users/dowonkim/Dropbox/data/celery_asodesign/mypkg/")
+from asopipe.main import run_ASOdesign#ASOdesign
 from app import celery
 
 
-@celery.task(bind=True)
-def run_asodesign(
-    self,
-    transid,
-    query_assemblies,
-    ref_assembly,
-    tile_length=18,
+@celery.task
+def run_asodesign_process(
+    transid="NM_002415",
+    query_assemblies=["mm39", "mm10"],
+    ref_assembly="hg38",
+    k_min=18,
+    k_max=20,
     chunk_division=5,
     max_workers=1,
     wobble=0,
-    gapmer_filtered=True,
-):
+    gapmer_filtered=True
+    ):
     mp.set_start_method("spawn", force=True)
     start = time.perf_counter()
-
-    aso = ASOdesign(
-        transid=transid,
-        query_assembly=query_assemblies,
-        ref_assembly=ref_assembly,
-        tile_length=tile_length,
+    chunk_division = 5
+    max_workers = 1
+    print(
+        f"transid: {transid}, query_assemblies: {query_assemblies}, ref_assembly: {ref_assembly}, "
+        f"k_min: {k_min}, k_max: {k_max}, chunk_division: {chunk_division}, max_workers: {max_workers}, wobble: {wobble}, gapmer_filtered: {gapmer_filtered}",
+        type(transid), type(query_assemblies), type(ref_assembly), type(k_min), type(k_max), type(chunk_division), type(max_workers), type(wobble), type(gapmer_filtered)
     )
-
-    df = aso.process_main(
-        chunk_division=chunk_division,
-        max_workers=max_workers,
-        wobble=wobble,
-        to_df=True,
-        gapmer_filtered=gapmer_filtered,
-    )
+    result  = run_ASOdesign(transid=transid,
+                    query_assembly=query_assemblies,
+                    ref_assembly=ref_assembly,
+                    k_min=k_min, k_max=k_max,
+                    chunk_division=chunk_division, max_workers=max_workers, wobble=wobble, to_df=False, gapmer_filtered=gapmer_filtered,
+                    to_csv=True, output_path=f"/Users/dowonkim/Dropbox/data/celery_asodesign/asoview/design_folder/")
+    print(result)
 
     return {
         "elapsed_sec": round(time.perf_counter() - start, 3),
-        "n_candidates": len(df),
-        "records": df.to_dict(orient="records"),
+        "records": result,
     }
+    
+    
